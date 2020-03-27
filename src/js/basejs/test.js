0b53f2906c74ad11facd00279f9e71da91d9027f
berb00 <berb11@163.com>
2020-03-27 21:04:15 +0800

    -

--- src/js/basejs/test.js
+++ src/js/basejs/test.js
@@ -1,12 +1,12 @@
 // vue schema
 // typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : (global = global || self, global.Vue = factory());
-
+// a?b:c?d:e?f:g means the same as a?b:(c?d:(e?f:g))
 (
     function (global, factory) {
 
-        typeof exports === 'object' && typeof module !== 'undefined' ?
-        module.exports = factory() : typeof define === 'function' && define.amd ?
-        define(factory) : (global = global || self, global.Vue = factory());
+        (typeof exports === 'object' && typeof module !== 'undefined') ? 
+        module.exports = factory() : 
+        (typeof define === 'function' && define.amd ? define(factory) : (global = global || self, global.Vue = factory()));
   
     }(
         this,
